6d775d65f157c052fe70a733171ba0d75a1495d6
Zachery Dickson <zachery.dickson@helsinki.fi>
2026-05-08 12:55:03 +0300

    V1.1.1 update - incorp changes from other branches, but revert away from some things added there

--- util/build_libs.sh
+++ util/build_libs.sh
@@ -3,9 +3,6 @@ cd "$projectDir" || exit 1;
 installDir="$projectDir/built_libs"
 mkdir -p "$installDir"
 htsLibVer=1.21
-igraphVer=1.0.1
-libleidenalgVer=0.12.0
-
 
 #Build HTSLib
 tar -xjf "htslib-$htsLibVer.tar.bz2"
@@ -15,24 +12,3 @@ make
 make install
 cd ..
 rm -rf "htslib-$htsLibVer"
-
-
-##Build igraph
-tar -xzf tar -xzf "igraph-$igraphVer.tar.gz"
-cd "igraph-$igraphVer"
-mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="$installDir" -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-cmake --build .
-cmake --install .
-cd ../..
-rm -rf "igraph-$igraphVer"
-
-#Build libleidenalg
-tar -xzf "libleidenalg-$libleidenalgVer.tar.gz" || exit 1;
-cd "libleidenalg-$libleidenalgVer" || exit 1;
-mkdir build && cd build || exit 1;
-cmake .. -DCMAKE_INSTALL_PREFIX="$installDir" -DCMAKE_PREFIX_PATH="$installDir"
-cmake --build .
-cmake --install .
-cd ../../
-rm -rf "libleidenalg-$libleidenalgVer"
